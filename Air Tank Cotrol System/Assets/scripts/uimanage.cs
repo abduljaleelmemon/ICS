16f13b3639f4fadea8aa682d82266cf050ad7e55
abduljaleelmemon <abduljaleelmemon3@gmail.com>
2020-12-19 04:30:51 +0500

    ACS

--- Air Tank Cotrol System/Assets/scripts/uimanage.cs
+++ Air Tank Cotrol System/Assets/scripts/uimanage.cs
@@ -5,11 +5,26 @@ using UnityEngine;
 public class uimanage : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject menudown;
+    public GameObject buttonDown;
+    int start = 0;
     void Start()
     {
         
     }
-
+    public void MoveUp() {
+        if (start == 0)
+        {
+            menudown.GetComponent<Animator>().Play("ui");
+            buttonDown.GetComponent<Animator>().Play("updown");
+            start = 1;
+        }
+        else {
+            start = 0;
+            menudown.GetComponent<Animator>().Play("uiDown");
+            buttonDown.GetComponent<Animator>().Play("downup");
+        }
+    }
     // Update is called once per frame
     void Update()
     {
