51e65a2db104f4f0da795e70e0400ee73a00bd01
abduljaleelmemon <abduljaleelmemon3@gmail.com>
2020-12-19 01:48:38 +0500

    heat-exc

---
+++ Heat Exchanger/Assets/CubeMover.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;	
+public class CubeMover : MonoBehaviour
+{	
+	
+	public float CurrentY = 0;
+	private float d = -1;
+	[SerializeField]
+	KeyCode keyUp;
+   [SerializeField]
+	KeyCode keyDown;
+	void Update(){
+	CurrentY = 360-transform.rotation.x;
+	   if(Input.GetKey(keyUp)) {
+		   if (CurrentY<360){
+		   Debug.Log("Pressed left click.");
+			transform.Rotate(d,0 ,0);
+			GameObject.FindGameObjectWithTag("MPin").GetComponent<Manometer>().RotateRight(gameObject,-2.3f);
+			GameObject.FindGameObjectWithTag("outerTank").GetComponent<steamIn>().OnSteamIn(gameObject,-1.45f);
+		   }
+	   }
+	   if(Input.GetKey(keyDown)){
+           if (CurrentY > 359.1474)
+           {
+		    Debug.Log("Pressed right click.");
+			transform.Rotate(-d,0 ,0);
+			GameObject.FindGameObjectWithTag("MPin").GetComponent<Manometer>().RotateLeft(gameObject,2.3f);
+			GameObject.FindGameObjectWithTag("outerTank").GetComponent<steamIn>().OnSteamIn(gameObject,1.45f);
+		   }
+	   }
+	}
+}
---
+++ Heat Exchanger/Assets/Manometer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manometer : MonoBehaviour
+{
+	void Update(){
+		
+	}
+	public float CurrentY = 0;
+	public void RotateRight(GameObject g,float d){
+		CurrentY = 360-transform.rotation.y; 
+		if(CurrentY>359.4961 )
+		 transform.Rotate(0 ,d,0, Space.Self); 
+	}
+	public void RotateLeft(GameObject g,float d){
+		CurrentY = 360-transform.rotation.y;
+		if(CurrentY<360.69 )
+		 transform.Rotate(0 ,d,0, Space.Self);
+	} 
+}
