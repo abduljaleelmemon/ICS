1a9bab870b0bd4fa487b3959309a97f99d206453
abduljaleelmemon <abduljaleelmemon3@gmail.com>
2020-12-20 01:42:36 +0500

    update-acs

---
+++ Air Tank Cotrol System/Assets/motorRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class motorRotation : MonoBehaviour
+{
+    public float moveSpeed = 6f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //transform.Rotate(Vector3.forward * 3 * Time.deltaTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward, moveSpeed );
+        //trasform.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+    }
+}
