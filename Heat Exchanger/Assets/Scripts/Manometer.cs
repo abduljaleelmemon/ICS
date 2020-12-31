5158748d9e437ff85f5ba943d2a6a00d450c66fb
abduljaleelmemon <abduljaleelmemon3@gmail.com>
2021-01-01 03:11:27 +0500

    update

---
+++ Heat Exchanger/Assets/Scripts/Manometer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Manometer : MonoBehaviour
+{
+	void Update(){
+		
+	}
+	public float currentY = 0;
+	public void RotateRight(GameObject g,float d){
+		currentY = 360-transform.rotation.y; 
+		if(currentY>359.4961 )
+		 transform.Rotate(0 ,d,0, Space.Self); 
+	}
+	public void RotateLeft(GameObject g,float d){
+		currentY = 360-transform.rotation.y;
+		if(currentY<360.69 )
+		 transform.Rotate(0 ,d,0, Space.Self);
+	} 
+}
---
+++ Heat Exchanger/Assets/Scripts/rotate.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+ 
+public class rotate : MonoBehaviour
+{
+ 
+    public float rotateSpeed = 180f;
+ 
+ 
+    // Use this for initialization
+    void Start () {
+ 
+    }
+ 
+    void Update () {
+ 
+    }
+ 
+    public void OnClickLeft () {
+       
+        transform.Rotate(Vector3.forward*rotateSpeed*Time.deltaTime);
+ 
+    }
+   
+}
